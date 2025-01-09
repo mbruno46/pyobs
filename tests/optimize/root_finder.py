414f715f45a6b86d67217d795c9e5e17fe7b731f
Mattia Bruno <38449948+mbruno46@users.noreply.github.com>
2025-01-10 00:19:59 +0100

    removed int as shortcut

--- tests/optimize/root_finder.py
+++ tests/optimize/root_finder.py
@@ -14,16 +14,16 @@ data = rng.markov_chain(y,dy**2,tau,N)
 yobs = pyobs.observable()
 yobs.create('test',data.flatten(),shape=(len(x),))
 
-int = pyobs.optimize.interpolate(x, yobs)
-print(int.coeff , numpy.array(aex))
-print(int([x[2]]), y[2])
+interp = pyobs.optimize.interpolate(x, yobs)
+print(interp.coeff , numpy.array(aex))
+print(interp([x[2]]), y[2])
 print('solve')
-print(int.solve(y[1], bracket=[x[0],x[2]]), x[1])
+print(interp.solve(y[1], bracket=[x[0],x[2]]), x[1])
 
 f = lambda x,a: a[0] + a[1]*x + a[2]*x*x - 0.3
 dfx = lambda x,a: a[1] + 2*a[2]*x
 dfa = lambda x,a: [1, x, x*x]
 
 print('\n root')
-x0 = pyobs.optimize.root_scalar(int.coeff, lambda x,a: f(x,a), dfx, dfa, bracket=[min(x),max(x)])
+x0 = pyobs.optimize.root_scalar(interp.coeff, lambda x,a: f(x,a), dfx, dfa, bracket=[min(x),max(x)])
 print(x0)
